e548c2048dcbcaac41264a53402a9d585a048632
Gyuo <gosim8611@gmail.com>
2021-09-22 16:10:23 +0900

    add loading API function

---
+++ src/modules/loading.js
@@ -0,0 +1,34 @@
+import { createAction, handleAction } from 'redux-actions';
+
+const START_LOADING = 'loading/START_LOADING';
+const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+/*
+ 요청을 위한 액션 타입을 payload로 설정합니다. (예: "sample/GET_POST")
+ 리듀서를 만든 다음에 루트 리듀서에도 등록합니다. modules/index.js
+ /gyuo/2021-09-22
+*/
+
+export const startLoading = createAction(
+  START_LOADING,
+  (requestType) => requestType,
+);
+
+export const finishLoading = createAction(
+  FINISH_LOADING,
+  (requestType) => requestType,
+);
+
+const initialState = {};
+
+const loading = handleAction(
+  {
+    [START_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: false,
+    }),
+  },
+  initialState,
+);
+
+export default loading;
